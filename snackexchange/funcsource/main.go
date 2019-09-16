2186b423fa856c71ab4e7b491dda20b7ff29b0de
Brian Ketelsen <bketelsen@gmail.com>
2019-09-16 19:43:52 -0400

    debug functions

--- snackexchange/funcsource/main.go
+++ snackexchange/funcsource/main.go
@@ -3,13 +3,30 @@ package main
 import (
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-lambda-go/lambdacontext"
+
+	"context"
 )
 
-func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+func handler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+lc, ok := lambdacontext.FromContext(ctx)
+	if !ok {
+		return events.APIGatewayProxyResponse{
+			StatusCode: 503,
+			Body:       "Something went wrong :(",
+		}, nil
+	}
+
+	cc := lc.ClientContext
+	var s string
+	for k := range cc.Custom {
+		s = s+ k + " : " + cc.Custom[k] + "\n" 
+	}
 	return events.APIGatewayProxyResponse{
 		StatusCode: 200,
-		Body:       "Hello AWS Lambda and Netlify",
-	}, nil
+		Body:       "Hello, " + s,
+		
+	}, nil	
 }
 
 func main() {
