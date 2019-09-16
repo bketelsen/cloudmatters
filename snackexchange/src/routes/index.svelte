6d95588fcb4ff1e08fa93b0c10b74de0e409050f
Brian Ketelsen <bketelsen@gmail.com>
2019-09-16 15:08:35 -0400

    try different go incantation

--- snackexchange/src/routes/index.svelte
+++ snackexchange/src/routes/index.svelte
@@ -36,6 +36,28 @@
 	<title>Sapper project template</title>
 </svelte:head>
 
+<script>
+import GoTrue from "gotrue-js";
+let email = ""
+let password = ""
+
+let auth = new GoTrue({
+  APIUrl: "https://optimistic-khorana-1a7154.netlify.com/.netlify/identity",
+  audience: "",
+  setCookie: false
+});
+
+
+function login() {
+auth
+  .login(email, password)
+  .then(response => {
+    console.log("Success! Response: " + JSON.stringify({ response }));
+  })
+  .catch(error => console.log("Failed :( " + JSON.stringify(error)));
+}
+</script>
+
 <h1>Great success!</h1>
 
 <figure>
@@ -44,3 +66,13 @@
 </figure>
 
 <p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+<form name="login">
+  <div class="message"></div>
+  <p>
+    <label>Email<br/><input type="email" name="email" bind:value={email} required/></label>
+  </p>
+  <p>
+    <label>Password<br/><input type="password" name="password" bind:value={password} required/></label>
+  </p>
+  <button type="submit" on:click={login}>Log in</button>
+</form>
